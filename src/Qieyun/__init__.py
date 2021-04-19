80576b5af39658ee2241b0d9de9c8a041bab61cd
Ayaka Mikazuki <ayaka@mail.shn.hk>
2021-04-19 11:07:04 +0800

    Update API for 韻圖

--- src/Qieyun/__init__.py
+++ src/Qieyun/__init__.py
@@ -2,5 +2,5 @@
 
 from .常量 import 常量
 from .音韻地位 import 音韻地位
-from .韻書 import 字頭2音韻地位_出處們, 音韻地位2代表字, 音韻地位2字頭_出處們, iter音韻地位
-from .韻圖 import 字頭_音韻地位2韻圖出處們
+from .韻書 import 音韻地位2代表字, 音韻地位2字頭_韻書出處們, iter音韻地位
+from .韻書和韻圖 import 字頭2音韻地位_出處們
