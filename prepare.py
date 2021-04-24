1a4c37a6179dfca4f1ff09f9b390b0f21781f734
Ayaka Mikazuki <ayaka@mail.shn.hk>
2021-04-24 09:25:10 +0800

    Update qieyun-data to 8bbd239

--- prepare.py
+++ prepare.py
@@ -2,7 +2,7 @@ from os import path
 from urllib.request import urlretrieve
 
 here = path.abspath(path.dirname(__file__))
-url = 'https://raw.githubusercontent.com/nk2028/qieyun-data/9c9c4d0/'
+url = 'https://raw.githubusercontent.com/nk2028/qieyun-data/8bbd239/'
 
 def retrieve(filename):
     target = path.join(here, 'src/Qieyun', filename)
