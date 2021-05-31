43a9549f69131c2ca298749f92feb2963235fa41
Ayaka Mikazuki <ayaka@mail.shn.hk>
2021-05-31 17:01:10 +0800

    Update links to book image

--- src/Qieyun/_書影.py
+++ src/Qieyun/_書影.py
@@ -12,7 +12,7 @@ def 生成書影(資料名稱, 圖片id, 縮略圖=False):
 
     if 資料名稱 == '王一':
         page = int(圖片id)
-        return f'https://coding-pages-bucket-3560900-8733724-16864-593420-1305783649.cos-website.ap-guangzhou.myqcloud.com/P2011/{縮略圖}{page:04}.jpg'
+        return f'https://cethiunn-1305783649.cos.ap-guangzhou.myqcloud.com/P2011/{縮略圖}{page:04}.jpg'
 
     if 資料名稱 == '廣韻':
         match = re.fullmatch(r'([^0-9]+)([0-9]+)([^0-9]+)', 圖片id)
@@ -20,14 +20,14 @@ def 生成書影(資料名稱, 圖片id, 縮略圖=False):
         # TODO FIXME: 此段程式碼對左右理解有誤
         page = int(match[2]) + [8, 2, 2, 4, 3][volume - 1]
         position = match[3]  # pylint: disable=unused-variable
-        return f'https://coding-pages-bucket-3560900-8733724-16866-593473-1305783649.cos-website.ap-guangzhou.myqcloud.com/deakdzuondangpuoon/volume{volume}/{縮略圖}p{page:03}.jpg'
+        return f'https://kuankhiunn-1305783649.cos.ap-guangzhou.myqcloud.com/deakdzuondangpuoon/volume{volume}/{縮略圖}p{page:03}.jpg'
 
     if 資料名稱 == '指微韻鑑（嘉吉本）':
         page = int(圖片id)
-        return f'https://coding-pages-bucket-3560900-8733724-16865-593472-1305783649.cos-website.ap-guangzhou.myqcloud.com/keakitpuoon/{縮略圖}{page+14:02}.jpg'
+        return f'https://hiunnkyanq-1305783649.cos.ap-guangzhou.myqcloud.com/keakitpuoon/{縮略圖}{page+14:02}.jpg'
 
     if 資料名稱 == '韻鏡（古逸叢書本）':
         page = int(圖片id)
-        return f'https://coding-pages-bucket-3560900-8733724-16865-593472-1305783649.cos-website.ap-guangzhou.myqcloud.com/kuujitdzungsjvpuoon/{縮略圖}{page+9:02}.png'
+        return f'https://hiunnkyanq-1305783649.cos.ap-guangzhou.myqcloud.com/kuujitdzungsjvpuoon/{縮略圖}{page+9:02}.png'
 
     raise NotImplementedError('未收錄指定書影')
